23e1cc01e0a79f9dfc57d49c7cc8b4ca5b088130
chumpon-asaneerat <chumponsenate@gmail.com>
2021-10-07 00:14:41 +0700

    Update 2021-10-07 #2 - Add sample expressjs server. - Add rest api test for url with or without ending route with slash.

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('My Choice Rater Web v2.0.9')
+const express = require('express')
+const morgan = require('morgan')
+
+let app = express()
+let port = 3000
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/users', (req, res) => {
+    res.send('Hello World! This is user url')
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
